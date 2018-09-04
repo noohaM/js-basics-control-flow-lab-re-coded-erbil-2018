50b14bf6cf4234e14bb0e44d45192d823fdd6c75
nooha mohammed <noohamohammed00@gmail.com>
2018-09-04 09:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function scuberGreetingForFeet(amo){
    }
    function switchOnCharmFromTip(tip){
       switch(tip){
-        case('generous'){return }
+        case('generous'){return "Thank you so much."}
       }
    }
    
