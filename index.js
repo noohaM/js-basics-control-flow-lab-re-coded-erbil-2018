669306022d35852caaf5358c7475429b2a1211f5
nooha mohammed <noohamohammed00@gmail.com>
2018-09-04 09:11:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(amo){
+  if( amo <= 400){
+    return "This one is on me!"
+     }else if(amo >= 2000 && amo < 2500){
+   	return "I will gladly take your thirty bucks."
+   }else{
+   	return 'No can do.'
+   }
+ }  
+   function ternaryCheckCity(city){
+    if(city == "NYC"){
+      return "Ok, sounds good."
+    }
+    else{
+      return "No go."
+    }
+   }
+   function switchOnCharmFromTip(){
+     
+   }
+   
--- test/indexTest.js
+++ test/indexTest.js
@@ -5,6 +5,7 @@ describe('index.js', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
     });
+    
 
     it('charges 30 dollars for a distance over 2000 feet', function () {
       expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
