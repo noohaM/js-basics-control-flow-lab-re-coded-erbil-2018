ffd7041ad26becd9fb8baec0a63fc9e58bfde1a4
nooha mohammed <noohamohammed00@gmail.com>
2018-09-04 09:22:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet(amo){
+  if( amo <= 400){
+    return "This one is on me!"
+     }else if(amo >= 2000 && amo < 2500){
+   	return "I will gladly take your thirty bucks."
+   }else{
+   	return 'No can do.'
+   }
+ }  
+   function ternaryCheckCity(city){
+    if(city == "NYC"){
+      return "Ok, sounds good."
+    }
+    else{
+      return "No go."
+    }
+   }
+   function switchOnCharmFromTip(tip){
+      switch(tip){
+        case"generous":return "Thank you so much.";
+        case"not as generous":return "Thank you.";
+        default: return "Bye."
+      }
+   }
+   
